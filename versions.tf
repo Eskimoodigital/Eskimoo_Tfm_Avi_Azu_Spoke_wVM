d68610615070d356a34c2cb7b911ad65139ce07a
Paul Carvill <85612526+Eskimoodigital@users.noreply.github.com>
2021-11-03 16:01:07 +0100

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -3,6 +3,10 @@ terraform {
     aviatrix = {
       source = "aviatrixsystems/aviatrix"
     }
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.83.0"
+    }
   }
   required_version = ">= 0.13"
 }
