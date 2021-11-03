101adf36ee277a6fe0550790fd8c248189bf3ee5
Paul Carvill <85612526+Eskimoodigital@users.noreply.github.com>
2021-11-03 17:07:45 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -81,17 +81,16 @@ resource "aviatrix_transit_firenet_policy" "default" {
 
 
 
-# resource "azurerm_resource_group" "example" {
-#   name     = "RGEskTfm"
-#   location = "West Europe"
-# }
+resource "azurerm_resource_group" "example" {
+  name     = "RGEskTfm"
+  location = "West Europe"
+}
 
 resource "azurerm_network_interface" "example" {
   name                = "example-nic"
-  #location            = azurerm_resource_group.example.location
-  #resource_group_name = azurerm_resource_group.example.name
-  location = var.region 
-  resource_group_name = aviatrix_vpc.resource_group_name.name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
 
   ip_configuration {
     name                          = "internal"
@@ -105,10 +104,9 @@ resource "azurerm_network_interface" "example" {
 
 resource "azurerm_public_ip" "example" {
   name                = "EskimooPublicIp1"
-  #resource_group_name = azurerm_resource_group.example.name
-  #location            = azurerm_resource_group.example.location
-  location = var.region
-  resource_group_name = aviatrix_vpc.resource_group_name.name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  
   allocation_method   = "Dynamic"
 
   }
@@ -116,10 +114,9 @@ resource "azurerm_public_ip" "example" {
 
 resource "azurerm_linux_virtual_machine" "example" {
   name                = "EskimooTest"
-  #resource_group_name = azurerm_resource_group.example.name
-  #location            = azurerm_resource_group.example.location
-  location = var.region
-  resource_group_name = aviatrix_vpc.resource_group_name.name
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+ 
   size                = "Standard_F2"
   disable_password_authentication = false
   admin_username      = "adminuser"
