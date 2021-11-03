e30694fa66eff4e2fe01f61f764bb540b518231e
Paul Carvill <85612526+Eskimoodigital@users.noreply.github.com>
2021-11-03 17:34:29 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -71,72 +71,62 @@ resource "aviatrix_transit_firenet_policy" "default" {
 
 
 
-
-
-
-
-
-
-
-
-
-
 resource "azurerm_resource_group" "example" {
   name     = "RGEskTfm"
   location = "West Europe"
 }
 
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+# resource "azurerm_network_interface" "example" {
+#   name                = "example-nic"
+#   location            = azurerm_resource_group.example.location
+#   resource_group_name = azurerm_resource_group.example.name
 
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = aviatrix_vpc.default[0].subnets[2].subnet_id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.example.id
+#   ip_configuration {
+#     name                          = "internal"
+#     subnet_id                     = aviatrix_vpc.default[0].subnets[2].subnet_id
+#     private_ip_address_allocation = "Dynamic"
+#     public_ip_address_id          = azurerm_public_ip.example.id
 
 
-  }
-}
+#   }
+# }
 
-resource "azurerm_public_ip" "example" {
-  name                = "EskimooPublicIp1"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+# resource "azurerm_public_ip" "example" {
+#   name                = "EskimooPublicIp1"
+#   resource_group_name = azurerm_resource_group.example.name
+#   location            = azurerm_resource_group.example.location
 
-  allocation_method = "Dynamic"
+#   allocation_method = "Dynamic"
 
-}
+# }
 
 
-resource "azurerm_linux_virtual_machine" "example" {
-  name                = "EskimooTest"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+# resource "azurerm_linux_virtual_machine" "example" {
+#   name                = "EskimooTest"
+#   resource_group_name = azurerm_resource_group.example.name
+#   location            = azurerm_resource_group.example.location
 
-  size                            = "Standard_F2"
-  disable_password_authentication = false
-  admin_username                  = "adminuser"
-  admin_password                  = "Password123!"
-  network_interface_ids = [
-    azurerm_network_interface.example.id,
-  ]
+#   size                            = "Standard_F2"
+#   disable_password_authentication = false
+#   admin_username                  = "adminuser"
+#   admin_password                  = "Password123!"
+#   network_interface_ids = [
+#     azurerm_network_interface.example.id,
+#   ]
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
+#   os_disk {
+#     caching              = "ReadWrite"
+#     storage_account_type = "Standard_LRS"
+#   }
 
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
-}
+#   source_image_reference {
+#     publisher = "Canonical"
+#     offer     = "UbuntuServer"
+#     sku       = "16.04-LTS"
+#     version   = "latest"
+#   }
+# }
 
 
 
