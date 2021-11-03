a83ba95a1f59696e8854e0f1ce80ccfbd6668e7d
Paul Carvill <85612526+Eskimoodigital@users.noreply.github.com>
2021-11-03 15:58:06 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -81,10 +81,62 @@ resource "aviatrix_transit_firenet_policy" "default" {
 
 
 
-# resource "azurerm_resource_group" "example" {
-#   name     = "RGEskTfm"
-#   location = "West Europe"
-# }
+resource "azurerm_resource_group" "example" {
+  name     = "RGEskTfm"
+  location = "West Europe"
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = aviatrix_vpc.default[0].subnets[2].subnet_id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.example.id
+
+    
+  }
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "EskimooPublicIp1"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  allocation_method   = "Dynamic"
+
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "EskimooTest"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_F2"
+  disable_password_authentication = false
+  admin_username      = "adminuser"
+  admin_password = "Password123!"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+}
+
+
+
 
 # resource "azurerm_virtual_network" "example" {
 #   name                = "example-network"
@@ -100,55 +152,3 @@ resource "aviatrix_transit_firenet_policy" "default" {
 #   address_prefixes     = ["10.78.0.0/24"]
 # }
 
-# resource "azurerm_network_interface" "example" {
-#   name                = "example-nic"
-#   location            = azurerm_resource_group.example.location
-#   resource_group_name = azurerm_resource_group.example.name
-
-#   ip_configuration {
-#     name                          = "internal"
-#     subnet_id                     = azurerm_subnet.example.id
-#     private_ip_address_allocation = "Dynamic"
-#     public_ip_address_id = azurerm_public_ip.example.id
-
-    
-#   }
-# }
-
-# resource "azurerm_public_ip" "example" {
-#   name                = "EskimooPublicIp1"
-#   resource_group_name = azurerm_resource_group.example.name
-#   location            = azurerm_resource_group.example.location
-#   allocation_method   = "Dynamic"
-
-#   tags = {
-#     environment = "Production"
-#   }
-# }
-
-# resource "azurerm_linux_virtual_machine" "example" {
-#   name                = "EskimooTest"
-#   resource_group_name = azurerm_resource_group.example.name
-#   location            = azurerm_resource_group.example.location
-#   size                = "Standard_F2"
-#   disable_password_authentication = false
-#   admin_username      = "adminuser"
-#   admin_password = "Password123!"
-#   network_interface_ids = [
-#     azurerm_network_interface.example.id,
-#   ]
-
-
-
-#   os_disk {
-#     caching              = "ReadWrite"
-#     storage_account_type = "Standard_LRS"
-#   }
-
-#   source_image_reference {
-#     publisher = "Canonical"
-#     offer     = "UbuntuServer"
-#     sku       = "16.04-LTS"
-#     version   = "latest"
-#   }
-# }
